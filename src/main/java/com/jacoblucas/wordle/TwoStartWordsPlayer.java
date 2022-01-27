a35c8a580c97189c2bce9b49b321651c3de90647
Jacob Lucas <jacobl@indeed.com>
2022-01-27 06:57:44 -0800

    Add TwoStartWordsPlayer

---
+++ src/main/java/com/jacoblucas/wordle/TwoStartWordsPlayer.java
@@ -0,0 +1,61 @@
+package com.jacoblucas.wordle;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import static com.jacoblucas.wordle.Game.WORD_LENGTH;
+import static com.jacoblucas.wordle.ResultCode.WRONG;
+
+public class TwoStartWordsPlayer extends Player {
+    final Map<Character, Long> charFrequencyMap = new HashMap<>();
+
+    public TwoStartWordsPlayer(final Game game) {
+        super(game);
+    }
+
+    @Override
+    public String getName() {
+        return "TwoStartWordsPlayer";
+    }
+
+    @Override
+    public String getNextWord() {
+        if (guessHistory.isEmpty()) {
+            // Start with a good guess
+            return "RAISE";
+        } else if (guessHistory.size() == 1) {
+            return "MOUTH";
+        }
+
+        // Prune letters from the guess order, and identify any wrong letters
+        final List<Character> availableLetters = GuessHistoryAnalysis.getAvailableLetters(guessHistory);
+        final List<Character> wrongLetters = GuessHistoryAnalysis.getLetters(guessHistory, WRONG);
+
+        // Identify any letters in the right location
+        String[] patternArr = new String[5];
+        for (Map.Entry<String, ResultCode[]> entry : guessHistory.entrySet()) {
+            String word = entry.getKey();
+            ResultCode[] resultCodes = entry.getValue();
+            for (int i = 0; i < WORD_LENGTH; i++) {
+                char letter = word.charAt(i);
+                ResultCode resultCode = resultCodes[i];
+                if (resultCode == ResultCode.RIGHT) {
+                    patternArr[i] = ""+letter;
+                } else {
+                    if (patternArr[i] == null) {
+                        patternArr[i] = "[" +
+                                availableLetters.stream().map(String::valueOf).sorted().collect(Collectors.joining())
+                                + "]";
+                    }
+                }
+            }
+        }
+
+        final Pattern pattern = Pattern.compile(String.join("", patternArr));
+        final List<String> options = Dictionary.getMatchingWords(pattern, wrongLetters);
+        return options.get(0);
+    }
+}
