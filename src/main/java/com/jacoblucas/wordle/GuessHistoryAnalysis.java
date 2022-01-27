4bfbe6e60336df0653231adfa497441381f0b673
Jacob Lucas <jacobl@indeed.com>
2022-01-27 06:57:14 -0800

    Small refactors

--- src/main/java/com/jacoblucas/wordle/GuessHistoryAnalysis.java
+++ src/main/java/com/jacoblucas/wordle/GuessHistoryAnalysis.java
@@ -1,6 +1,9 @@
 package com.jacoblucas.wordle;
 
+import com.google.common.collect.Sets;
+
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -11,21 +14,16 @@ import static com.jacoblucas.wordle.Player.ALPHABET;
 public class GuessHistoryAnalysis {
 
     public static List<Character> getAvailableLetters(final LinkedHashMap<String, ResultCode[]> guessHistory) {
-        final List<Character> availableLetters = ALPHABET.chars().mapToObj(i -> (char) i).collect(Collectors.toList());
-        guessHistory.forEach((word, resultCodes) -> {
-            for (int i = 0; i < WORD_LENGTH; i++) {
-                char letter = word.charAt(i);
-                ResultCode resultCode = resultCodes[i];
-                if (resultCode == ResultCode.NEVER) {
-                    availableLetters.remove(new Character(letter));
-                }
-            }
-        });
-
-        return availableLetters;
+        return new ArrayList<>(
+                Sets.difference(
+                        ALPHABET.chars().mapToObj(ch -> (char) ch).collect(Collectors.toSet()),
+                        new HashSet<>(getLetters(guessHistory, ResultCode.NEVER))));
     }
 
-    public static List<Character> getLetters(final LinkedHashMap<String, ResultCode[]> guessHistory, final ResultCode resultCode) {
+    public static List<Character> getLetters(
+            final LinkedHashMap<String, ResultCode[]> guessHistory,
+            final ResultCode resultCode
+    ) {
         final List<Character> wrongLetters = new ArrayList<>();
         guessHistory.forEach((word, resultCodes) -> {
             for (int i = 0; i < WORD_LENGTH; i++) {
